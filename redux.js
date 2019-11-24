fd86c76a769b67766ae2fa0b1e3481fa18b3d9b7
Danis Zaidan <daniszaidan@gmail.com>
2019-11-24 21:49:44 +0700

    Dispatching Action Script

--- redux.js
+++ redux.js
@@ -12,3 +12,7 @@ const rootReducer = (state = this.initialState, action) => { return state }
 // Store
 const store = createStrore(rootReducer);
 console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({ type: 'ADD_AGE' });
+store.dispatch({ type: 'CHANE_VALUE', newValue: 12 });
