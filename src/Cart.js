7ee5c8606234edd8fd1db07a566474d833db31e8
Danis Zaidan <daniszaidan@gmail.com>
2019-11-26 14:25:32 +0700

    Create Cart Component

---
+++ src/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Cart() {
+    return (
+        <div className="cart">
+            <p>cart : <span>10</span></p>
+        </div>
+    );
+}
+
+export default Cart;
