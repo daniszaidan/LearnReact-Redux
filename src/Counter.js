82232798687fc0ff5924fe3fd6375de8e7e9c173
Danis Zaidan <daniszaidan@gmail.com>
2019-11-26 14:25:15 +0700

    Create Counter Component

---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Counter() {
+    return (
+        <div className="counter">
+            <button>-</button>
+            <p>10</p>
+            <button>+</button>
+        </div>
+    );
+}
+
+export default Counter;
