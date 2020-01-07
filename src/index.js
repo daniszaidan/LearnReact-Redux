904a8e5dd8f6d61e47e5672ef8fba1f3fe0248e6
Danis Zaidan <daniszaidan@gmail.com>
2020-01-07 11:27:51 +0700

    create Redux, Reducer, and use Provider

--- src/index.js
+++ src/index.js
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const globalState = {
+    totalOrder: 0
+}
+
+// Reducer 
+const rootReducer = (state = globalState, action) => {
+    switch (action.type) {
+        case 'PLUS_ORDER':
+            return {
+                ...state,
+                totalOrder: state.totalOrder + 1
+            }
+        case 'MINUS_ORDER':
+            if (state.totalOrder != 0) {
+                return {
+                    ...state,
+                    totalOrder: state.totalOrder - 1
+                }
+            }
+        default:
+            return state;
+    }
+}
+
+// Store
+const storeRedux = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={storeRedux}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
