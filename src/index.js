7c2dc9d0f501b2c8f41dc6b7a7ab3b2299b2e52b
Danis Zaidan <daniszaidan@gmail.com>
2020-01-08 14:16:44 +0700

    add Redux Thunk in project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
 import rootReducer from "./redux/reducer/globalReducer";
 
 // Store
-const storeRedux = createStore(rootReducer);
+const storeRedux = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={storeRedux}><App /></Provider>, document.getElementById('root'));
 
